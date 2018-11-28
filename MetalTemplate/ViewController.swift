bb1b87d6b9076082ecfb79da067e0f7f819c5793
Joss Manger <joss.manger@langlandusa.com>
2018-11-28 15:28:08 -0500

    Initial Commit

---
+++ MetalTemplate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalTemplate
+//
+//  Created by Joss Manger on 11/28/18.
+//  Copyright © 2018 Joss Manger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
