3384964423442ee9e5ad3f99ef24c90a175419a5
Joss Manger <Jwam3rd@gmail.com>
2018-12-04 07:03:25 -0500

    adds gesture recognizers to hype train iOS target

--- HypeTrain/ViewController.swift
+++ HypeTrain/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
     var renderer:Renderer!
     
+    let panSlowFactor:CGFloat = 20
+    let slowFactor:CGFloat = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,9 +25,25 @@ class ViewController: UIViewController {
         
         renderer = Renderer(metalView: metalView)
         
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(gesture(_:)))
+        metalView.addGestureRecognizer(pinch)
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(gesture(_:)))
+        metalView.addGestureRecognizer(pan)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @objc func gesture(_ sender:UIGestureRecognizer) -> Void {
+        if(sender is UIPanGestureRecognizer){
+            let pan = (sender as! UIPanGestureRecognizer)
+            let velocity = pan.velocity(in: view)
+            renderer.rotationDegs -= Float(velocity.x / panSlowFactor)
+            renderer.yRotationDegs += Float(velocity.y / panSlowFactor)
+        } else if sender is UIPinchGestureRecognizer{
+            let pinch = sender as! UIPinchGestureRecognizer
+            let point = pinch.velocity / slowFactor
+            renderer.zScale += Float(point)
+        }
+    }
 
 }
 
