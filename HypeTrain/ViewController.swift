a6d00e3977fd48fe764b80b0636c138fef6d6204
Joss Manger <Jwam3rd@gmail.com>
2018-12-04 08:13:47 -0500

    adds tap for triangle vs point rendering, refactors gesture handle code into switch

--- HypeTrain/ViewController.swift
+++ HypeTrain/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
 
     var renderer:Renderer!
     
+    //divisors to slow the movement from gesture recognisers
     let panSlowFactor:CGFloat = 20
     let slowFactor:CGFloat = 10
+    
+    //render triangles?
+    var triangles:Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,25 +28,38 @@ class ViewController: UIViewController {
         }
         
         renderer = Renderer(metalView: metalView)
+        // Do any additional setup after loading the view, typically from a nib.
         
+        //Create and add gesture recognisers
         let pinch = UIPinchGestureRecognizer(target: self, action: #selector(gesture(_:)))
         metalView.addGestureRecognizer(pinch)
         let pan = UIPanGestureRecognizer(target: self, action: #selector(gesture(_:)))
         metalView.addGestureRecognizer(pan)
-        // Do any additional setup after loading the view, typically from a nib.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(gesture(_:)))
+        metalView.addGestureRecognizer(tap)
+        
     }
 
     @objc func gesture(_ sender:UIGestureRecognizer) -> Void {
-        if(sender is UIPanGestureRecognizer){
+        
+        //function responds to multiple gesture recognisers, switch to determine which code to run
+        switch sender {
+        case _ where sender is UITapGestureRecognizer:
+            triangles = !triangles
+            renderer.setTriangles(drawTriangles: triangles)
+        case _ where sender is UIPanGestureRecognizer:
             let pan = (sender as! UIPanGestureRecognizer)
             let velocity = pan.velocity(in: view)
             renderer.rotationDegs -= Float(velocity.x / panSlowFactor)
             renderer.yRotationDegs += Float(velocity.y / panSlowFactor)
-        } else if sender is UIPinchGestureRecognizer{
+        case _ where sender is UIPinchGestureRecognizer:
             let pinch = sender as! UIPinchGestureRecognizer
             let point = pinch.velocity / slowFactor
             renderer.zScale += Float(point)
+        default:
+            break
         }
+        
     }
 
 }
